f51bd65fc733528246a413f77d1746927105f504
Harpreet Kaur <HK022@atcltd.atc.org.nz>
2020-11-12 13:14:34 +1300

    Add project files.

---
+++ MoviesApi/Migrations/20201027171202_movie.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoviesApi.Migrations
+{
+    public partial class movie : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movie",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Rating = table.Column<int>(nullable: false),
+                    Released = table.Column<string>(nullable: true),
+                    Plot = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movie", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movie");
+        }
+    }
+}
---
+++ MoviesApi/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesApi.Models
+{
+    public class Movie // model class
+    {
+        // PK
+        [Key]
+        public int Id { get; set; }
+        // name of the movie
+        public string Name { get; set; }
+        // imdb rating
+        public int Rating { get; set; }
+        // released date
+        public string Released { get; set; }
+        // a small plot about movie
+        public string Plot { get; set; }
+
+    }
+}
